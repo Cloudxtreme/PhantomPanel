5ea03e581d12171a0262f995257f1ac48a80895d
gmt2001 <garymt2001@hotmail.com>
2015-07-15 05:37:49 -0500

    Added a missing PUT usage

--- controlpanel.php
+++ controlpanel.php
@@ -1,5 +1,13 @@
 <?php require_once('includes/includes.php'); ?>
+<?php
+if (isset($_POST['message'])) {
+    $result = curl_put($_POST['message']);
+}
 
+if (isset($_POST['message2']) && isset($_POST['message3'])) {
+    $result = curl_put($_POST['message2'] . $_POST['message3']);
+}
+?>
 <!DOCTYPE html>
 <html>
 
