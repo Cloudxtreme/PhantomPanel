b0ec83a6137009c231a4beb70bc1c22f48549dec
Alex De Arnas <thephantomindex@gmail.com>
2015-07-23 02:43:40 -0400

    This icon was overlooked

--- modules/botcommands.php
+++ modules/botcommands.php
@@ -35,7 +35,7 @@ if (isset($_POST['message2']) && isset($_POST['message3'])) {
             <div class="col-lg">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
-					<h3 class="panel-title" style="height: 25px;"><i class="fa fa-android fa-2x" style="float:left;">&nbsp;&nbsp; </i>
+					<h3 class="panel-title" style="height: 25px;"><i class="fa fa-pencil-square-o" style="float:left;">&nbsp;&nbsp; </i>
 						<div id="title" style="float:left;padding-top:3px;">Title: <span style="color:#a68ed2;" id="stream_title"></span></div>
 						<div id="game" style="float:right;padding-top:3px;">Game: <span  style="color:#a68ed2;" id="stream_game"></span></div>
 					</h3>
